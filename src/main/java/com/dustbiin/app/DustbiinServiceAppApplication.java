44c46591dd2ec38b4f8a05904bd1fc708f871df2
dustbiin <iamabhishekmaurya@gmail.com>
2021-07-27 10:28:29 +0530

    Initial Commit

---
+++ src/main/java/com/dustbiin/app/DustbiinServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.dustbiin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DustbiinServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DustbiinServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dustbiin/app/DustbiinServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustbiin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustbiinServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
